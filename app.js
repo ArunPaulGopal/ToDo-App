c51a7082a3f223b7476aecdcd6d9aaa6004139cf
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-04-15 15:48:16 -0700

    Add new To-Do feature complete.

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ app.controller("HomeCtrl",home);
 app.$inject = ['$http'];
 
 function home($http){
-  var that = this;
-  that.greeting = "Hello";
-  that.world = "World";
+  var vm = this;
+  vm.greeting = "Hello";
+  vm.world = "World";
   var user = $http.get('http://localhost:1337/user');
   user.then(function(info){
-    that.user =info.data.name
+    vm.user =info.data.name
   })
 };
 
@@ -19,9 +19,36 @@ app.controller("todoController",todo);
 app.$inject = ['$http'];
 
 function todo($http) {
-  var that = this;
-  var todos = $http.get('http://localhost:1337/todos/Arun')
-  todos.then(function(todo){
-    that.list = todo.data;
-  })
+  var vm = this;
+
+  function initialLoad() {
+    getTodos()
+  }
+  initialLoad();
+
+  function getTodos() {
+    var todos = $http.get('http://localhost:1337/read')
+    todos.then(function(todo){
+      console.log(todo.data);
+      vm.list = todo.data;
+    })
+  }
+
+
+  vm.finished = function(item) {
+    var position = vm.list.indexOf(item);
+    vm.list.splice(position,1);
+  }
+
+
+
+  vm.add = function (content){
+    var todo = {};
+    todo.content = content;
+    var added = $http.post('http://localhost:1337/create',todo)
+    added.then(function(){
+      getTodos()
+    })
+  }
+
 }
